3ad5516eb7f05863f471421cc388a5c2b023acc3
sruthi-002 <89520373+sruthi-002@users.noreply.github.com>
2022-08-03 16:55:12 +0530

    Add files via upload

---
+++ deletefile.java
@@ -0,0 +1,31 @@
+﻿package com.company;
+import java.io.*;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.io.IOException;
+class deletefile
+{
+    public static void delete() throws IOException , FileNotFoundException
+
+    {
+        try {
+            File myfile = new File("C:\\Users\\ELCOT\\untitled1");
+            boolean b = myfile.delete();
+            if (b)
+                System.out.println("File is Deleted");
+            else
+                System.out.println("File is not Deleted");;
+        } catch (Exception e) {
+            System.out.println("Given file name is wrong");
+        }
+    }
+}
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+	deletefile myobj = new deletefile();
+    myobj.delete();
+    }
+}
